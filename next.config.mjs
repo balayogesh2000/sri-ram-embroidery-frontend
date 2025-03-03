bfe1e206c64de1ce1c63d9f429215591afb9ac73
Bala Yogesh <balayogesh2000@gmail.com>
2025-03-03 18:56:06 +0530

    added new s3 domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["gandhiram-embroidery.s3.ap-south-1.amazonaws.com"],
+    domains: ["gandhiram-embroidery-dev.s3.ap-south-1.amazonaws.com"],
   },
 };
 
